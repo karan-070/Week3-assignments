148547d8ba93411e8e17500177f43f08270ff9ff
karan-070 <karanjeet.singh@clarivate.com>
2023-10-26 15:53:49 +0530

    Uploaded

---
+++ Exercise2/CustomDependancy/src/main/java/org/example/CustomLibrary.java
@@ -0,0 +1,8 @@
+package org.example;
+
+
+public class CustomLibrary {
+    public int square(int number) {
+        return number * number;
+    }
+}
---
+++ Exercise2/DependencyManagement/feature-module/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        CustomLibrary customLibrary = new CustomLibrary();
+        int result = customLibrary.square(5);
+        System.out.println("Square: " + result);
+    }
+}
